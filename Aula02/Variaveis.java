025fdf2d03643f94605da63ede42e0cc85d2b578
Nycollas009 <snycollas30@gmail.com>
2026-03-16 09:24:13 -0300

    Aula sobre variaveis

---
+++ Aula02/Variaveis.java
@@ -0,0 +1,15 @@
+package Aula02;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        String msgPt1 = "Olá";
+        String msgPt2 = "Alunos";
+        System.out.println(msgPt1.concat(msgPt2));
+        
+        int n1 = 1;
+        String n2 = "2";
+        System.out.println(n1 + Integer.parseInt(n2));
+        
+    }
+    
+}
